1df3f07ace64bf0c991738e5d585f119014310b8
venir.dev <luca@venir.dev>
2023-05-23 10:14:42 +0200

    Added some documentation to `Location`

--- lib/src/current_weather/domain/entities/location.dart
+++ lib/src/current_weather/domain/entities/location.dart
@@ -4,7 +4,13 @@ class Location {
     required this.lat,
     required this.long,
   });
+
+  /// Latitude of this place
   final double lat;
+
+  /// Longitude of this place
   final double long;
+
+  /// A human-readable name of this place
   final String name;
 }
