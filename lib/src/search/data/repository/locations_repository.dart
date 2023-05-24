03d967d5695819783b0eb44c95c37bc089c6104d
LeoDaVeenchy <arturoromanelli13@gmail.com>
2023-05-24 11:55:30 +0200

    Added location repository

---
+++ lib/src/search/data/repository/locations_repository.dart
@@ -0,0 +1,15 @@
+import '../dto/locations_dto.dart';
+import '../sources/locations_api.dart';
+
+class LocationsRepository {
+  const LocationsRepository(this.api);
+  final LocationsApi api;
+
+  Future<LocationsDto> getLocations(String search) async {
+    final result = await api.locations(
+      q: search,
+    );
+
+    return LocationsDto.fromJson(result);
+  }
+}
