80691e9dc1f16311f9936d9199c220e70bb9a8c4
venir.dev <luca@venir.dev>
2023-05-25 17:57:09 +0200

    Fixed the image problems

--- lib/src/current_weather/README.md
+++
@@ -1,3 +0,0 @@
-# Current weather
-
-This feature allows the user to check the current weather in its city.
--- lib/src/current_weather/domain/adapters/current_weather_from_dto.dart
+++ lib/src/current_weather/domain/adapters/current_weather_from_dto.dart
@@ -2,10 +2,15 @@ import '../../data/dto/current_weather_dto.dart';
 import '../entities/current_weather.dart';
 
 extension CurrentWeatherFromDto on CurrentWeatherDto {
-  CurrentWeather toDomain() => CurrentWeather(
-        temp: current.tempC.toInt(),
-        weather: current.condition.text,
-        updatedAt: current.lastUpdated,
-        image: current.condition.icon,
-      );
+  CurrentWeather toDomain() {
+    final truncatedUrl = current.condition.icon.substring(2);
+    final imageUrl = 'https://$truncatedUrl';
+
+    return CurrentWeather(
+      temp: current.tempC.toInt(),
+      weather: current.condition.text,
+      updatedAt: current.lastUpdated,
+      image: imageUrl,
+    );
+  }
 }
