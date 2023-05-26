5ded7986e998389687344758c4717825266af32d
venir.dev <luca@venir.dev>
2023-05-26 15:35:06 +0200

    Added a provider observer

--- lib/main.dart
+++ lib/main.dart
@@ -2,7 +2,13 @@ import 'package:flutter/material.dart';
 import 'package:hooks_riverpod/hooks_riverpod.dart';
 
 import 'core/weather_app.dart';
+import 'logs/provider_observer.dart';
 
 void main() {
-  runApp(const ProviderScope(child: WeatherApp()));
+  runApp(
+    const ProviderScope(
+      observers: [MyProviderObserver()],
+      child: WeatherApp(),
+    ),
+  );
 }
