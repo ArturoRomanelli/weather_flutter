4ff346427cd534c080a6b73f918de5ca0373a746
LeoDaVeenchy <arturoromanelli13@gmail.com>
2023-05-27 10:59:46 +0200

    changed import to relative

--- lib/src/forecast/domain/adapters/forecast_day_from_dto.dart
+++ lib/src/forecast/domain/adapters/forecast_day_from_dto.dart
@@ -1,8 +1,10 @@
-import 'package:weather_flutter/src/forecast/domain/entities/forecast_day.dart';
-
 import '../../data/dto/forecast_day_dto.dart';
+import '../entities/forecast_day.dart';
 
 extension ForecastDayFromDto on ForecastDayDto {
-  ForecastDay toDomain() =>
-      ForecastDay(temp: day.avgtempC, weather: day.condition.text, date: date);
+  ForecastDay toDomain() => ForecastDay(
+        temp: day.avgtempC,
+        weather: day.condition.text,
+        date: date,
+      );
 }
