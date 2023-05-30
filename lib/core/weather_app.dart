8f4351bbe060888d2cb680a0984b31bb16cd07db
venir.dev <luca@venir.dev>
2023-05-30 17:14:23 +0200

    Configured theme and fonts for our application

--- lib/core/weather_app.dart
+++ lib/core/weather_app.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:weather_flutter/theme/dark_theme.dart';
+import 'package:weather_flutter/theme/light_theme.dart';
 
 import '../router/router.dart';
 
@@ -9,6 +11,8 @@ class WeatherApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp.router(
       routerConfig: router,
+      theme: lightTheme,
+      darkTheme: darkTheme,
     );
   }
 }
